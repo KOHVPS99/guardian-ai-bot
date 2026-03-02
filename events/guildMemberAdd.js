7d0af65c182377dddc841a1201dc12be7211e946
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-03-02 16:42:51 +0400

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,9 @@
+// events/guildMemberAdd.js
+const antiRaid = require("../systems/antiRaid");
 
+module.exports = {
+  name: "guildMemberAdd",
+  async execute(client, member) {
+    await antiRaid(member);
+  },
+};
