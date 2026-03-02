a6b32ab68c7b9fcf3ae2ce04b13a455189e3325a
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-03-02 16:43:43 +0400

    Update roleDelete.js

--- events/roleDelete.js
+++ events/roleDelete.js
@@ -1,1 +1,10 @@
+// events/roleDelete.js
+const antiNuke = require("../systems/antiNuke");
 
+module.exports = {
+  name: "roleDelete",
+  async execute(client, role) {
+    if (!role.guild) return;
+    await antiNuke.handleRoleDelete(client, role);
+  },
+};
