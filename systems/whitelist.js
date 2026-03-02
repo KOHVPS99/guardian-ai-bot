a75e8bb16bfddf8c2093324026b76da46e177539
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-03-02 16:39:39 +0400

    Update whitelist.js

--- systems/whitelist.js
+++ systems/whitelist.js
@@ -1,1 +1,15 @@
+// systems/whitelist.js
+function isWhitelisted(member, config) {
+  if (!member) return false;
+  if (config.WHITELIST_USERS?.includes(member.id)) return true;
 
+  const roleIds = config.WHITELIST_ROLES || [];
+  if (roleIds.length > 0 && member.roles?.cache) {
+    for (const rid of roleIds) {
+      if (member.roles.cache.has(rid)) return true;
+    }
+  }
+  return false;
+}
+
+module.exports = { isWhitelisted };
