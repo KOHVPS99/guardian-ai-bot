eb73884a3d9cbccf72e3e247e3df5e860b346d4c
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-03-02 16:43:21 +0400

    Update channelDelete.js

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,1 +1,10 @@
+// events/channelDelete.js
+const antiNuke = require("../systems/antiNuke");
 
+module.exports = {
+  name: "channelDelete",
+  async execute(client, channel) {
+    if (!channel.guild) return;
+    await antiNuke.handleChannelDelete(client, channel);
+  },
+};
