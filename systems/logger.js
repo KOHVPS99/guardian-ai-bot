952c5b258097fb7eb862664a11ce33e2b878348e
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-03-02 16:39:28 +0400

    Update logger.js

--- systems/logger.js
+++ systems/logger.js
@@ -1,1 +1,17 @@
+// systems/logger.js
+module.exports = {
+  async log(client, guild, message) {
+    try {
+      const { LOG_CHANNEL_ID } = client.ctx.config;
+      if (!LOG_CHANNEL_ID || LOG_CHANNEL_ID.includes("PUT_")) return;
 
+      const ch = guild.channels.cache.get(LOG_CHANNEL_ID);
+      if (!ch) return;
+
+      await ch.send(message);
+    } catch (e) {
+      // don't crash bot because of logging
+      console.error("logger.log error:", e);
+    }
+  },
+};
