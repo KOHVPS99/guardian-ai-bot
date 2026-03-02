c857147b9b6ab9bddb2ccce44d8819240c2765fc
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-03-02 16:43:13 +0400

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,1 +1,10 @@
+// events/channelCreate.js
+const antiNuke = require("../systems/antiNuke");
 
+module.exports = {
+  name: "channelCreate",
+  async execute(client, channel) {
+    if (!channel.guild) return;
+    await antiNuke.handleChannelCreate(client, channel);
+  },
+};
