c5a50c883c23bf28f0ed2ebb88046fc57268ad3b
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-03-02 16:43:33 +0400

    Update roleCreate.js

--- events/roleCreate.js
+++ events/roleCreate.js
@@ -1,1 +1,10 @@
+// events/roleCreate.js
+const antiNuke = require("../systems/antiNuke");
 
+module.exports = {
+  name: "roleCreate",
+  async execute(client, role) {
+    if (!role.guild) return;
+    await antiNuke.handleRoleCreate(client, role);
+  },
+};
